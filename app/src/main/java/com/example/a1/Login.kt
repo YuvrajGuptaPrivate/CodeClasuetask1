6a38e06f8728a4a74e35e101f00ac85fd26d8628
CC42233 <138650975+CC42233@users.noreply.github.com>
2024-06-29 15:32:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/a1/Login.kt
@@ -0,0 +1,66 @@
+package com.example.a1
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+
+class Login : AppCompatActivity() {
+     lateinit var firebaseAuth: FirebaseAuth
+    var etEmailAddress = findViewById<EditText>(R.id.etEmailAddress)
+    var etPassword=findViewById<EditText>(R.id.etPassword)
+    var btnLogin=findViewById<Button>(R.id.btnLogin)
+    var btnSignUp=findViewById<Button>(R.id.btnSignUp)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        firebaseAuth = FirebaseAuth.getInstance()
+        btnLogin.setOnClickListener {
+            login()
+        }
+
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+
+    private fun login(){
+        val email = etEmailAddress.text.toString()
+        val password = etPassword.text.toString()
+
+
+        if (email.isBlank() || password.isBlank()) {
+            Toast.makeText(this, "Email/password cannot be empty", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this){
+            if(it.isSuccessful){
+                Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            else{
+                Toast.makeText(this, "Authentication Failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/a1/SignUp.kt
@@ -0,0 +1,74 @@
+package com.example.a1
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+
+class SignUp : AppCompatActivity() {
+    lateinit var firebaseAuth: FirebaseAuth
+    var etEmailAddress = findViewById<EditText>(R.id.etEmailAddress)
+    var etPassword=findViewById<EditText>(R.id.etPassword)
+    var btnLogin=findViewById<Button>(R.id.btnLogin)
+    var btnSignUp=findViewById<Button>(R.id.btnSignUp)
+    var etConfirmPassword=findViewById<EditText>(R.id.etConfirmPassword)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sign_up)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        btnSignUp.setOnClickListener {
+            signUpUser()
+        }
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun signUpUser(){
+        val email = etEmailAddress.text.toString()
+        val password = etPassword.text.toString()
+        val confirmPassword = etConfirmPassword.text.toString()
+
+        if (email.isBlank() || password.isBlank() || confirmPassword.isBlank()) {
+            Toast.makeText(this, "Email and Password can't be blank", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (password != confirmPassword) {
+            Toast.makeText(this, "Password and Confirm Password do not match", Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) {
+                if(it.isSuccessful){
+                    Toast.makeText(this,"Login Successful", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+                else{
+                    Toast.makeText(this, "Error creating user.", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+}
+
